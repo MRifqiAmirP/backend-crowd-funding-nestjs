4dbd8268c5a58bfdab5c6398526f5f064eb1305a
baloerrr <akbaradityah444@gmail.com>
2025-07-02 21:03:32 +0700

    feat: user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,14 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+
+})
+export class UserModule { }
