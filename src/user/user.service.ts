1ae9afac3a7ebf7bb78ad41fc41192ad8cd949e9
baloerrr <akbaradityah444@gmail.com>
2025-07-02 21:03:24 +0700

    feat: user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    private prisma: PrismaService
+  ) { }
+
+  async create(data: CreateUserDto) {
+    return await this.prisma.user.create({ data });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOneByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: string, data: UpdateUserDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!existingUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    return await this.prisma.user.update({
+      where: { id },
+      data: {
+        ...data,
+        updated_at: new Date(),
+      },
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.user.delete({
+      where: {
+        id
+      }
+    });
+  }
+}
