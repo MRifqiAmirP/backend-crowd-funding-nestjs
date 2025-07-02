a6bb92284b7874d43a3a33937753dac5d0fbdb47
baloerrr <akbaradityah444@gmail.com>
2025-07-02 21:00:25 +0700

    feat: cookie parser, global pipeline for validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+      }));
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
