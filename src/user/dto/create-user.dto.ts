065479b206e24bfe5bb5662993b4386674c61f94
baloerrr <akbaradityah444@gmail.com>
2025-07-03 08:05:10 +0700

    fix: email_validated field

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,36 @@
-import {} from 'class-validator'
+import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class CreateUserDto {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: "min 8 characters" })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   first_name: string;
+
+  @IsString()
   last_name?: string;
+
+  @IsString()
+  @IsNotEmpty()
   role: string;
-  email_validated: boolean;
+
+  @IsString()
+  @IsNotEmpty()
   instance: string;
+
+  @IsString()
+  @IsNotEmpty()
   education_level: string;
+
+  @IsBoolean()
+  @IsOptional()
+  email_validated?: boolean;
 }
