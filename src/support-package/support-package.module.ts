5748d649845577b8609e0ff838762806a5c22dc4
baloerrr <akbaradityah444@gmail.com>
2025-07-07 22:34:47 +0700

    feat(suppor-package): imprts prisma module and jwt module

--- src/support-package/support-package.module.ts
+++ src/support-package/support-package.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupportPackageService } from './support-package.service';
 import { SupportPackageController } from './support-package.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [PrismaModule, JwtModule],
   controllers: [SupportPackageController],
   providers: [SupportPackageService],
 })
