ab4516fe8c7390d2f65aa620c68b2be9efcaa49c
Muhammad Rifqi Amir Putra <muhammadrifqiamirp20@gmail.com>
2025-07-07 22:24:08 +0700

    feat(deploy): update pm2 command to start application without restart

--- src/support-package/support-package.module.ts
+++ src/support-package/support-package.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupportPackageService } from './support-package.service';
 import { SupportPackageController } from './support-package.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [SupportPackageController],
   providers: [SupportPackageService],
 })
