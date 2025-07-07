944b8a7e7f19754fa425aedb5ce292cdb95e8dac
baloerrr <akbaradityah444@gmail.com>
2025-07-07 21:49:31 +0700

    feat(app): add midtrans module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,10 @@ import { GalleryModule } from './gallery/gallery.module';
 import { CommentarProjectModule } from './commentar-project/commentar-project.module';
 import { BlogModule } from './blog/blog.module';
 import { SupportPackageModule } from './support-package/support-package.module';
+import { MidtransModule } from './midtrans/midtrans.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ProjectModule, CategoryModule, GalleryModule, CommentarProjectModule, BlogModule, SupportPackageModule],
+  imports: [PrismaModule, UserModule, AuthModule, ProjectModule, CategoryModule, GalleryModule, CommentarProjectModule, BlogModule, SupportPackageModule, MidtransModule],
   controllers: [AppController],
   providers: [AppService],
 })
