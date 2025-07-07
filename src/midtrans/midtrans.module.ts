51af72a6d9ad0bfa53bc4add210656a0e2205bb4
Muhammad Rifqi Amir Putra <muhammadrifqiamirp20@gmail.com>
2025-07-07 22:48:55 +0700

    feat(database): add initial migration and update package.json scripts

--- src/midtrans/midtrans.module.ts
+++ src/midtrans/midtrans.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MidtransService } from './midtrans.service';
 import { MidtransController } from './midtrans.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [MidtransController],
   providers: [MidtransService],
 })
