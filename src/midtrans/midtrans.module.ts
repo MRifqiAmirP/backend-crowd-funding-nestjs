be2d337f3cab8d9d34cfe3203da8ddca29fecec2
baloerrr <akbaradityah444@gmail.com>
2025-07-08 08:24:00 +0700

    feat(midtrans): add funding service and user service on providers midtrans module

--- src/midtrans/midtrans.module.ts
+++ src/midtrans/midtrans.module.ts
@@ -3,11 +3,13 @@ import { MidtransService } from './midtrans.service';
 import { MidtransController } from './midtrans.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { FundingService } from 'src/funding/funding.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [MidtransController],
-  providers: [MidtransService],
+  providers: [MidtransService, FundingService, UserService],
   exports: [MidtransService]
 })
 export class MidtransModule {}
