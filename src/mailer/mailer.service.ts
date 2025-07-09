a06a60bd33db0288e662b83710774407a117f199
baloerrr <akbaradityah444@gmail.com>
2025-07-09 11:27:20 +0700

    feat(mailer): add send and send reset password into mailer service

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService as NestMailerService } from '@nestjs-modules/mailer';
+
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+    constructor(
+        private mailerService: NestMailerService
+    ) { }
+
+    async send(to: string, subject: string, template: string, context: Record<string, any>) {
+        await this.mailerService.sendMail({
+            to,
+            subject,
+            template,
+            context,
+        });
+    }
+
+    async sendResetPassword(email: string, token: string) {
+        const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${token}`;
+
+        await this.mailerService.sendMail({
+            to: email,
+            subject: 'Reset Your Password',
+            template: './reset-password', // gunakan ejs/hbs template
+            context: {
+                resetLink,
+            },
+        });
+    }
+}
