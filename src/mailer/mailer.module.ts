78517f740cb05706bc5377db855ce752f7be9f10
baloerrr <akbaradityah444@gmail.com>
2025-07-09 11:26:43 +0700

    feat(mailer): make singleton mailer

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { MailerService } from './mailer.service';
-import { MailerController } from './mailer.controller';
+import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as path from 'path';
+
 
 @Module({
-  controllers: [MailerController],
+  imports: [
+    NestMailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+      template: {
+        dir: path.join(process.cwd(), 'src', 'mailer', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   providers: [MailerService],
+  exports: [MailerService],
 })
 export class MailerModule {}
